366e644a5a173ed2b1bed932fe031cc444ea6c84
hanneka <98503316+hanneka@users.noreply.github.com>
2023-02-04 12:07:37 +0800

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ReferencesComponent } from './references/references.component';
+import { AchievementsComponent } from './achievements/achievements.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, AboutComponent, EducationComponent, SkillsComponent,ReferencesComponent, AchievementsComponent, PagenotfoundComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
